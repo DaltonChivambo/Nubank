6e5810c2a01c2814e1cd9920205eee4e991183cb
dalton <daltonchivambo@gmail.com>
2022-11-06 00:55:35 +0200

    add 2nd stage

--- app/src/main/java/com/skyink/nubank/MainActivity.kt
+++ app/src/main/java/com/skyink/nubank/MainActivity.kt
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.skyink.nubank.adapter.AdapterPayment
+import com.skyink.nubank.adapter.AdapterProduct
 import com.skyink.nubank.databinding.ActivityMainBinding
 import com.skyink.nubank.model.Payment
+import com.skyink.nubank.model.Product
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapterPayment: AdapterPayment
+    private lateinit var adapterProduct: AdapterProduct
     private val paymentList: MutableList<Payment> = mutableListOf()
+    private val productList: MutableList<Product> = mutableListOf()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar!!.hide()
+
+
         //define layout manager for recycle view
         val recyclerIconPayment = binding.recyclerIconPayments
         recyclerIconPayment.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
@@ -29,6 +35,14 @@ class MainActivity : AppCompatActivity() {
         adapterPayment = AdapterPayment(this,paymentList)
         recyclerIconPayment.adapter = adapterPayment
         paymentIconList()
+
+
+        val recyclerProduct = binding.recyclerProducts
+        recyclerProduct.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+        recyclerProduct.setHasFixedSize(true)
+        adapterProduct = AdapterProduct(this,productList)
+        recyclerProduct.adapter = adapterProduct
+        informativeText()
     }
 
     private fun paymentIconList(){
@@ -57,5 +71,19 @@ class MainActivity : AppCompatActivity() {
         paymentList.add(donation)
 
 
+    }
+
+    private fun informativeText(){
+        val textPJAccount = Product("Get to know the PJ account: practical and free of bureaucracy to...")
+        productList.add(textPJAccount)
+
+        val textPromotion = Product("Participate in the All in Purple promotion and win...")
+        productList.add(textPromotion)
+
+        val textAutoDebit = Product("Auto-debit card invoice arrived")
+        productList.add(textAutoDebit)
+
+        val textSaveFriend = Product("Save your friends for the bureaucracy: invite...")
+        productList.add(textSaveFriend)
     }
 }
