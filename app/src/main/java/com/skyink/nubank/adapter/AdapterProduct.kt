312da22b4de18057986a04ae51bd7e6225e9fb58
dalton <daltonchivambo@gmail.com>
2022-11-05 23:56:16 +0200

    add AdapterProduct.kt

--- app/src/main/java/com/skyink/nubank/adapter/AdapterProduct.kt
+++ app/src/main/java/com/skyink/nubank/adapter/AdapterProduct.kt
@@ -1,4 +1,31 @@
 package com.skyink.nubank.adapter
 
-class AdapterProduct {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.skyink.nubank.databinding.ProductItemBinding
+import com.skyink.nubank.model.Product
+
+class AdapterProduct(private val context: Context, private val productList: MutableList<Product>) :
+    RecyclerView.Adapter<AdapterProduct.ProductViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
+        val itemList = ProductItemBinding.inflate(LayoutInflater.from(context),parent,false)
+        return ProductViewHolder(itemList)
+    }
+
+    override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ProductViewHolder(binding: ProductItemBinding): RecyclerView.ViewHolder(binding.root) {
+        val txtInformation = binding.txtInformation
+    }
 }
---
+++ app/src/main/java/com/skyink/nubank/model/Product.kt
@@ -0,0 +1,5 @@
+package com.skyink.nubank.model
+
+data class Product (
+    val text: String? = null
+        )
